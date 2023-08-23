1e0fac9feac58ae1175c881717163c3a1acf7dcb
HeavenOSK <heavenosk@gmail.com>
2023-08-24 01:53:32 +0900

    Initial Commit

---
+++ SwiftUITutorial1/SwiftUITutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorial1App.swift
+//  SwiftUITutorial1
+//
+//  Created by 渡邉龍之介 on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
